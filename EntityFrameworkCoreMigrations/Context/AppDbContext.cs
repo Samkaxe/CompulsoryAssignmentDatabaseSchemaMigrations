d2c6a606089a93be52989100290671d144a37a61
Samkaxe <must2765@easv365.dk>
2024-03-05 17:44:45 +0100

    seting up readme file

--- EntityFrameworkCoreMigrations/Context/AppDbContext.cs
+++ EntityFrameworkCoreMigrations/Context/AppDbContext.cs
@@ -8,6 +8,7 @@ public class AppDbContext : DbContext
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<ProductRating> ProductRatings { get; set; }
+    public DbSet<Tests> Tests { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlite("Data Source=app.db");
